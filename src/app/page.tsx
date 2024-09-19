2a8b943ee2de53bbea19394dfdfe813dbd65b85a
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-09-19 09:20:10 +0300

    Change sign in Link style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header';
 
 export default function Home() {
   return (
-    <main>
+    <main className='h-screen overflow-y-hidden bg-gradient-to-b from-white to-purple-100'>
       <Header />
       <BackgroundBeamsWithCollisionDemo />
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 function Header() {
   return (
-    <nav className='mx-auto max-w-6xl px-8'>
-      <div className='flex justify-between p-2'>
+    <nav className='mx-auto max-w-6xl sm:px-8 sm:pb-0 pb-10'>
+      <div className='flex justify-between items-center p-2'>
         <Image src={blob} alt='logo' width='100' height='100'></Image>
-        <Link href={'/signIn'} className='btn text-zinc-800'>sign in</Link>
+        <Link href={'/signIn'} className='btn text-zinc-800 outline-purple-400 outline bg-white hover:bg-zinc-100'>Sign in</Link>
       </div>
     </nav>
   )
